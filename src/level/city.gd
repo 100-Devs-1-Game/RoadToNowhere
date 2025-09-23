b3d8780e8055d2609c7bcab1e8997f61a9d2207e
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-23 23:04:52 +0200

    Spawn neighbor asphalt tiles

--- src/level/city.gd
+++ src/level/city.gd
@@ -4,6 +4,7 @@ extends Node2D
 enum TileLayer { GROUND, BUILDINGS, OBJECTS, ROADS }
 
 @export var tilemaps: Array[TileMapLayer]
+@export var asphalt_tile: FloorTile
 
 
 
@@ -12,9 +13,19 @@ func _ready() -> void:
 
 
 func place_tile(tile_to_place: BaseTile, tile_pos: Vector2i, rotation: Vector2i= Vector2i.ZERO):
-	var tilemap: TileMapLayer= get_tilemap(tile_to_place.target_tilemap)
+	var target_tilemap: TileLayer= tile_to_place.target_tilemap
+	var tilemap: TileMapLayer= get_tilemap(target_tilemap)
+
 	tilemap.set_cell(tile_pos, tile_to_place.source_id, Vector2i.ZERO)
 
+	if target_tilemap == TileLayer.BUILDINGS or target_tilemap == TileLayer.ROADS:
+		place_tile(asphalt_tile, tile_pos)
+		if target_tilemap == TileLayer.BUILDINGS:
+			for neighbor_pos in get_neighbor_tiles(tile_pos):
+				if is_water_tile(neighbor_pos):
+					continue
+				place_tile(asphalt_tile, neighbor_pos)
+ 
 
 func get_tilemap(type: TileLayer)-> TileMapLayer:
 	return tilemaps[int(type)]
@@ -32,5 +43,37 @@ func get_tile_from_position(pos: Vector2)-> Vector2i:
 	return tilemaps[0].local_to_map(pos)
 
 
+func get_neighbor_tiles(center: Vector2i, include_center: bool= false)-> Array[Vector2i]:
+	var result: Array[Vector2i]
+	for x in range(-1, 2):
+		for y in range(-1, 2):
+			var neighbor:= center + Vector2i(x, y)
+			if not is_in_bounds(neighbor):
+				continue
+			if include_center or neighbor != center:
+				result.append(neighbor)
+	
+	return result
+
+
+func get_floor_tile(tile_pos: Vector2i)-> FloorTile:
+	var source_id: int= get_source_id(tile_pos, TileLayer.GROUND)
+	if source_id == -1:
+		return null
+	return GameData.source_id_to_ground_tile[source_id]
+
+
+func get_source_id(tile: Vector2i, tile_layer: TileLayer)-> int:
+	var tile_map: TileMapLayer= get_tilemap(tile_layer)
+	return tile_map.get_cell_source_id(tile)
+
+
+func is_water_tile(tile: Vector2i)-> bool:
+	var floor_tile: FloorTile= get_floor_tile(tile)
+	if not floor_tile:
+		return false
+	return floor_tile.is_water
+
+
 func is_in_bounds(tile: Vector2i)-> bool:
-	return true
+	return tile in tilemaps[0].get_used_cells()
