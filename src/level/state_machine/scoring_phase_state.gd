395fb8af58b5ca604180bbc7e536e00eb90a7b0f
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-24 07:16:49 +0200

    Update scoring_phase_state.gd

--- src/level/state_machine/scoring_phase_state.gd
+++ src/level/state_machine/scoring_phase_state.gd
@@ -9,31 +9,27 @@ func on_enter():
 
 func score_city():
 	var city: City= Global.city
-	var tilemap: TileMapLayer= city.get_tilemap(City.TileLayer.ROADS)
-	var origin: Vector2= tilemap.get_global_transform_with_canvas().origin
-	origin.y-= 20
 
-	var road_dict: Dictionary
-	for tile in tilemap.get_used_cells():
-		FloatingText.add(origin + city.get_position_from_tile(tile), "+1", 2.0, Color.GREEN, 30, false, true)
-		for x in range(-1, 2):
-			for y in range(-1, 2):
-				var pos:= tile + Vector2i(x, y)
-				road_dict[pos]= true
+	var road_access_dict: Dictionary
+	for tile_pos in city.get_road_tiles():
+		#FloatingText.add(origin + city.get_position_from_tile(tile), "+1", 2.0, Color.GREEN, 30, false, true)
 		
-		for connection: Vector2i in city.get_road_connections(tile):
-			var connected_road_pos: Vector2i= tile + connection
-			if connected_road_pos not in tilemap.get_used_cells() or -connection not in city.get_road_connections(connected_road_pos):
-				FloatingText.add(origin + city.get_position_from_tile(tile) + Vector2(connection) * 32, "-1", 2.0, Color.RED, 30, false, true)
-					
+		trigger_score(tile_pos, 1)
+		for pos in Utils.get_neighbor_tiles(tile_pos, false): 
+			road_access_dict[pos]= true
+		
+		for connection: Vector2i in city.get_road_connections(tile_pos):
+			var connected_road_pos: Vector2i= tile_pos + connection
+			if connected_road_pos not in city.get_road_tiles() or -connection not in city.get_road_connections(connected_road_pos):
+				#FloatingText.add(origin + city.get_position_from_tile(tile) + Vector2(connection) * 32, "-1", 2.0, Color.RED, 30, false, true)
+				trigger_score(tile_pos, -1, Vector2(connection) * 32)
 		
 		await get_tree().create_timer(0.5).timeout
 
-
-	tilemap= city.get_tilemap(City.TileLayer.BUILDINGS)
-	for tile in tilemap.get_used_cells():
-		if road_dict.has(tile):
-			FloatingText.add(origin + city.get_position_from_tile(tile), "+5", 2.0, Color.GREEN, 30, false, true)
+	for tile_pos in city.get_building_tiles():
+		if road_access_dict.has(tile_pos):
+			#FloatingText.add(origin + city.get_position_from_tile(tile), "+5", 2.0, Color.GREEN, 30, false, true)
+			trigger_score(tile_pos, 1)
 		await get_tree().create_timer(0.5).timeout
 
 
@@ -44,3 +40,17 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if event.keycode == KEY_F1:
 			score_city()
+
+
+func trigger_score(tile: Vector2i, amount: int, offset: Vector2= Vector2.ZERO):
+	assert(amount != 0)
+	var city: City= Global.city
+	var origin: Vector2= city.get_global_canvas_transform()
+	origin.y-= 20
+	
+	var color:= Color.GREEN
+	if amount < 0:
+		color= Color.RED
+	
+	FloatingText.add(origin + city.get_position_from_tile(tile) + offset, Utils.number_with_sign(amount), 2.0, color, 30, false, true)
+	
