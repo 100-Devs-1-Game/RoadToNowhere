eb30ad9c4e612bba5e0451d46833e370016e46f1
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-25 19:48:31 +0200

    Add buy button

--- src/deck_builder/card_container.gd
+++ src/deck_builder/card_container.gd
@@ -1,9 +1,21 @@
 class_name CardContainer
 extends PanelContainer
 
+signal buy_card(data: CardData)
+
 @onready var card: Card = %Card
+@onready var spin_box: SpinBox = %SpinBox
+@onready var button_buy: Button = %"Button Buy"
+
 
 
 func init(data: CardData):
 	card.init(data)
-	
+	spin_box.visible= data.unlocked
+	button_buy.visible= not data.unlocked
+	if button_buy.visible:
+		button_buy.text= str("$", data.cost)
+
+
+func _on_button_buy_pressed() -> void:
+	buy_card.emit(card.data)
