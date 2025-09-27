580c0af80e95b24f6b04da0e6a1debd9131a2165
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-27 08:50:37 +0200

    Update deck.gd

--- src/cards/deck.gd
+++ src/cards/deck.gd
@@ -15,6 +15,13 @@ func add_card(data: CardData):
 	cards.append(DeckCard.new(data))
 
 
+func copy()-> Deck:
+	var new_deck:= Deck.new()
+	for card in cards:
+		new_deck.add_card(card.data)
+	return new_deck
+
+
 func clear():
 	cards.clear()
 
@@ -26,6 +33,12 @@ func shuffle():
 func pop()-> CardData:
 	var card: DeckCard= cards.pop_back()
 	return card.data
+
+
+func half():
+	var half_size: int= int(cards.size() / 2)
+	while cards.size() > half_size:
+		cards.pop_back()
 
 
 func get_size()-> int:
