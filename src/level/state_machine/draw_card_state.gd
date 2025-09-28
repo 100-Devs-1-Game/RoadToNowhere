b1cb8600dc86b041aeba3060b0e854c2a1ff9077
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-28 23:42:34 +0200

    Add skip draw card toggle

--- src/level/state_machine/draw_card_state.gd
+++ src/level/state_machine/draw_card_state.gd
@@ -3,6 +3,14 @@ extends StateMachineState
 
 signal drawn_card(tile_to_place: CardData)
 
+@export var skip: bool= true
+
+
+
+func on_enter():
+	if skip:
+		drawn_card.emit(Global.level.draw_card())
+
 
 func on_card_drawn():
 	if is_current_state():
