71f9c85ea624caabbc626f5ebc8745a59a787d10
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-28 03:45:59 +0200

    Replace SpinBox

--- src/deck_builder/card_container.gd
+++ src/deck_builder/card_container.gd
@@ -4,8 +4,13 @@ extends PanelContainer
 signal bought_card(data: CardData)
 
 @onready var card: Card = %Card
-@onready var spin_box: SpinBox = %SpinBox
 @onready var button_buy: Button = %"Button Buy"
+@onready var label: Label = %Label
+@onready var button_plus: Button = %"Button Plus"
+@onready var button_minus: Button = %"Button Minus"
+@onready var spin_box: HBoxContainer = %"HBoxContainer Spin Box"
+
+var prev_value: int= 0
 
 
 
@@ -16,12 +21,24 @@ func init(data: CardData):
 	if button_buy.visible:
 		button_buy.text= str("$", data.cost)
 		button_buy.disabled= Player.money < data.cost
-	
-	await get_tree().process_frame
-	spin_box.scale*= 2
+
+
+func change_label(delta: int):
+	var value: int= int(label.text)
+	value+= delta
+	label.text= str(value)
+	button_minus.disabled= value == 0
 
 
 func _on_button_buy_pressed() -> void:
 	card.data.unlocked= true
 	Player.buy(card.data.cost)
 	bought_card.emit(card.data)
+
+
+func _on_button_minus_pressed() -> void:
+	change_label(-1)
+
+
+func _on_button_plus_pressed() -> void:
+	change_label(1)
