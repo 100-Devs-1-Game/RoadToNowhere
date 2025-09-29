1ae5ba985c67618ef7eacd759f0cb77bcc7d7488
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-29 08:43:24 +0200

    Improve skipping

--- src/level/state_machine/draw_card_state.gd
+++ src/level/state_machine/draw_card_state.gd
@@ -4,16 +4,18 @@ extends StateMachineState
 signal drawn_card(tile_to_place: CardData)
 
 @export var skip: bool= true
+@export var skip_delay: float= 0.3
 
 
 
 func on_enter():
 	if skip:
 		await get_tree().process_frame
+		await get_tree().create_timer(skip_delay).timeout
 		drawn_card.emit(Global.level.draw_card())
 
 
 func on_card_drawn():
-	if is_current_state():
+	if not skip and is_current_state():
 		drawn_card.emit(Global.level.draw_card())
 		return
