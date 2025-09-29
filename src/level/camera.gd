9a7ed7305056b89a55376b73ca65354efdbdad83
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-29 09:52:46 +0200

    Clamp camera

--- src/level/camera.gd
+++ src/level/camera.gd
@@ -1,9 +1,14 @@
 extends Camera2D
 
-@export var pan_speed: float= 300.0
-
+@export var pan_speed: float= 500.0
+@export var clamp_range: float= 300.0
 
 
 func _process(delta: float) -> void:
 	var pan: Vector2= Input.get_vector("pan_left", "pan_right", "pan_up", "pan_down")
-	position+= pan * pan_speed * delta
+	var pan_move: Vector2= pan.round() * pan_speed * delta
+	var new_position: Vector2= position + pan_move
+	if abs(new_position.x) < clamp_range:
+		position.x= new_position.x
+	if abs(new_position.y) < clamp_range:
+		position.y= new_position.y
